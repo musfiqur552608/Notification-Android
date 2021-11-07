91af9f9cb71d53671c637f71033ffeadde4a6684
Musfiq <mushfiqur15-10872@diu.edu.bd>
2021-11-08 04:07:06 +0600

    Customised

--- app/src/main/java/com/example/notificaton/MainActivity.java
+++ app/src/main/java/com/example/notificaton/MainActivity.java
@@ -11,6 +11,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -33,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void sendChannel1(View v){
+    public void sendChannel1(View v) {
         String title1 = title.getText().toString();
         String message1 = message.getText().toString();
         Intent activityIntent = new Intent(this, MainActivity.class);
@@ -41,14 +43,21 @@ public class MainActivity extends AppCompatActivity {
                 0, activityIntent, 0);
 
         Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage",message1);
+        broadcastIntent.putExtra("toastMessage", message1);
         PendingIntent actionIntent = PendingIntent.getBroadcast(this,
-                0,broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.dog);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle(title1)
                 .setContentText(message1)
+                .setLargeIcon(largeIcon)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.long_dummy_text))
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentIntent(contentIntent)
@@ -57,17 +66,28 @@ public class MainActivity extends AppCompatActivity {
                 .setColor(Color.BLUE)
                 .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build();
-        notificationManagerCompat.notify(1,notification);
+        notificationManagerCompat.notify(1, notification);
     }
-    public void sendChannel2(View v){
+
+    public void sendChannel2(View v) {
         String title1 = title.getText().toString();
         String message1 = message.getText().toString();
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
                 .setContentTitle(title1)
                 .setContentText(message1)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("This is line 1")
+                        .addLine("This is line 2")
+                        .addLine("This is line 3")
+                        .addLine("This is line 4")
+                        .addLine("This is line 5")
+                        .addLine("This is line 6")
+                        .addLine("This is line 7")
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .build();
-        notificationManagerCompat.notify(2,notification);
+        notificationManagerCompat.notify(2, notification);
     }
 }
